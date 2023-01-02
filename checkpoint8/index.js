a3ca79b8db65f5238d843cbc0b884180afa0228b
Randa Kourifa <randa.kourifa@gmail.com>
2023-01-02 12:41:35 +0100

    checkpoint8

---
+++ checkpoint8/index.js
@@ -0,0 +1,13 @@
+function simpleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(simpleSort([8, 4, 5, 2, 9, 3, 1]));
