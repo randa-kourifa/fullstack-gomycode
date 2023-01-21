fadea903f5b9f2e17548a22486a542ad6c5a4585
Randa Kourifa <randa.kourifa@gmail.com>
2023-01-21 16:20:19 +0100

    checkpoint91

--- checkpoint9/checkpoint9/src/components/Form/Form.jsx
+++ checkpoint9/checkpoint9/src/components/Form/Form.jsx
@@ -4,7 +4,7 @@ export default function Form() {
   return (
     <form className={module.secret}>
       <div class="form-group">
-        <label for="exampleInput">Tell me a secret</label>
+        <label for="exampleInput">Tell me a secret !</label>
         <input type="text" class="form-control" id="exampleInput" />
         <small id="note" class="form-text text-muted">
           We'll never share your secret with anyone else.
