7ef9df7da3138f21e9d0b34851da31703494496d
Randa Kourifa <randa.kourifa@gmail.com>
2023-01-21 16:11:40 +0100

    checkpoint9

---
+++ checkpoint9/checkpoint9/src/components/Form/Form.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import module from "./Form.module.css";
+export default function Form() {
+  return (
+    <form className={module.secret}>
+      <div class="form-group">
+        <label for="exampleInput">Tell me a secret</label>
+        <input type="text" class="form-control" id="exampleInput" />
+        <small id="note" class="form-text text-muted">
+          We'll never share your secret with anyone else.
+        </small>
+      </div>
+
+      <button type="submit" class="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
