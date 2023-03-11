5e0813a111b487755277555cffe119c1b1223c44
Randa Kourifa <randa.kourifa@gmail.com>
2023-03-11 16:47:47 +0100

    un truc

--- checkpoint13/checkpoint13/src/App.jsx
+++ checkpoint13/checkpoint13/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Descriptionandtrailer from "./pages/descriptionandtrailer";
+import Descriptionandtrailer from "./pages/Descriptionandtrailer";
+
 import Home from "./pages/Home";
 function App() {
   return (
