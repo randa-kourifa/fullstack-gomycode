6bc4491ce1ee0f784b603e591ef7dd67b7548940
Randa Kourifa <randa.kourifa@gmail.com>
2023-03-11 16:38:02 +0100

    changes13

--- checkpoint13/checkpoint13/src/pages/Descriptionandtrailer.jsx
+++ checkpoint13/checkpoint13/src/pages/Descriptionandtrailer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 export default function Descriptionandtrailer() {
   return (
     <div>
-      <link path="/Homes">go back to home pages</link>
+      <link path="/Homes">go back home pages</link>
     </div>
   );
 }
